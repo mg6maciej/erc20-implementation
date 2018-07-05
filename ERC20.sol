4c20e8b9a4ac8688d12b21482cf82e875ce86117
Maciej Górski <maciek.gorski@gmail.com>
2018-07-05 17:56:56 +0200

    Make deployment cheaper and transfers safer.

--- ERC20.sol
+++ ERC20.sol
@@ -5,15 +5,15 @@ contract ERC20 {
     event Transfer(address indexed from, address indexed to, uint amount);
     event Approval(address indexed owner, address indexed spender, uint amount);
 
-    uint private MAX_UINT = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF;
+    uint constant private MAX_UINT = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF;
 
     mapping (address => uint) private balances;
     mapping (address => mapping (address => uint)) private allowed;
 
-    string public name = "ERC20 Token";
-    string public symbol = "ERC20";
-    uint public decimals = 18;
-    uint public totalSupply = 10000 * (10 ** decimals);
+    string constant public name = "ERC20 Token";
+    string constant public symbol = "ERC20";
+    uint constant public decimals = 18;
+    uint constant public totalSupply = 10000 * (10 ** decimals);
 
     constructor(address owner) public {
         balances[owner] = totalSupply;
@@ -29,7 +29,8 @@ contract ERC20 {
     }
 
     function transfer(address to, uint amount) external returns (bool) {
-        require(balances[msg.sender] >= amount);
+        require(to != 0);
+        require(amount <= balances[msg.sender]);
         balances[msg.sender] -= amount;
         balances[to] += amount;
         emit Transfer(msg.sender, to, amount);
@@ -37,9 +38,10 @@ contract ERC20 {
     }
 
     function transferFrom(address from, address to, uint amount) external returns (bool) {
+        require(to != 0);
         uint allowedForMsgSender = allowed[from][msg.sender];
-        require(allowedForMsgSender >= amount);
-        require(balances[from] >= amount);
+        require(amount <= allowedForMsgSender);
+        require(amount <= balances[from]);
         balances[from] -= amount;
         balances[to] += amount;
         if (allowedForMsgSender != MAX_UINT) {
